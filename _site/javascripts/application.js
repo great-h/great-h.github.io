7bc0c6054201250da15e9e47a9b2d2baa7e21639
Tomohiko Himura <eiel.hal@gmail.com>
2014-02-19 02:01:14 +0900

    _sites ディレクトリを削除

--- _site/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-// add target about to outer link
-(function(){
-    $(function(){
-        $("a[href^='http']:not([href^='" + location.protocol + "//" + location.host + "'])").attr('target', '_blank');
-    });
-})();
